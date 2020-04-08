5dced2f8768e87313e0ddc812f22673df2c531e4
Mik0aj <mikoajmoson@gmail.com>
2020-04-08 11:27:34 +0200

    halko2

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -18,6 +17,7 @@ public class Main extends Application {
         primaryStage.show();
     }
 //halko
+    //halko2
 
     public static void main(String[] args) {
         launch(args);
